da8dbb36879026b4198426c3b6a1c67f34be7b4b
Edward Rees <erees.hk@gmail.com>
2026-07-22 19:32:15 -0700

    [feat] started ui

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main class="bg-slate-800 text-slate-100 min-h-screen min-w-screen px-5">
+<h1 class="py-5 text-3xl text-center">Welcome to this simple group system!</h1>
+<p class="text-xl text-center">Log in to get started!</p>
+</main>
